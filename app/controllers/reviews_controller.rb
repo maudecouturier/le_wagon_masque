205dea0e42d61f11928408938f577661b30b5269
Pauline Tolila <pauline.tolila@gmail.com>
2020-02-17 18:14:35 +0100

    modif controllers

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,27 @@
+class ReviewsController < ApplicationController
+# a faire avec les views
+#   def new
+#     @booking = Booking.find(params[:id])
+#     @review = Review.new
+#   end
+
+#   def create
+#     @review = Review.new(review_params)
+#     @review.user = current_user
+#     @review.booking = @booking
+#     if @review.save
+#       redirect_to booking_path(@booking)
+#     else
+#       render :new
+#     end
+#   end
+
+#   def destroy
+#   end
+
+#   private
+
+#   def review_params
+#     params.require(:review).permit(:content)
+#   end
+end
