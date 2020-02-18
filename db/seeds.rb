f1c316d8e01f438390c215eb675b0c43ca1baff7
Pauline Tolila <pauline.tolila@gmail.com>
2020-02-18 18:11:00 +0100

    modification seed & ajout dependent destroy

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,21 +14,27 @@ julien = User.create(email:"julien@glewagon.com", password:"lewagon")
 puts "Users created!"
 
 puts "Creating costumes..."
-pirate = Costume.create!(user: jeanne, description: "Déguisement d'homme pirate", location: "Paris", price: 20, size: "M", gender: "H", theme: "adulte")
 file_pirate = URI.open("https://i.pinimg.com/originals/ac/2c/35/ac2c3590123dd55231cb038b9b7e3528.jpg")
+pirate = Costume.new(title:"pirate", user: jeanne, description: "Déguisement d'homme pirate", location: "Paris", price: 20, size: "M", gender: "M", theme: "pirates")
 pirate.photo.attach(io: file_pirate, filename: 'pirate.jpg', content_type: 'image/jpg')
+pirate.save!
 
-princesse = Costume.create!(user: lea, description: "déguisement de conte de fée sexy", location: "Limoges", price: 15, size: "S", gender: "F", theme: "adulte")
+
 file_princesse = URI.open("https://www.deguiz-fetes.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/d/e/deguisement-princesse-blanche-neige-sexy.jpg")
+princesse = Costume.new(title:"princesse", user: lea, description: "déguisement de conte de fée sexy", location: "Limoges", price: 15, size: "S", gender: "F", theme: "fairy-tales")
 princesse.photo.attach(io: file_princesse, filename: 'princesse.jpg', content_type: 'image/jpg')
+princesse.save!
 
-mouton = Costume.create!(user: jeanne, description: "déguisement mouton bébé", location: "Lille", price: 15, size: "18 mois", gender: "M", theme: "enfant")
 file_mouton = URI.open("https://i.pinimg.com/564x/78/89/c5/7889c5ee96b95a0432bb3214398e08ff.jpg")
+mouton = Costume.new(title:"mouton", user: jeanne, description: "déguisement mouton bébé", location: "Lille", price: 15, size: "XS", gender: "M", theme: "animals")
 mouton.photo.attach(io: file_mouton, filename: 'mouton.jpg', content_type: 'image/jpg')
+mouton.save!
 
-wagon = Costume.create!(user: julien, description: "déguisement pour les soirées du wagon", location: "paris", price: 30, size: "M", gender: "M", theme: "adulte")
 file_wagon = URI.open("https://media.monsieurdeguisement.com/media/catalog/product/cache/3/image/9df78eab33525d08d6e5fb8d27136e95/d/i/disfraz-de-furgoneta-hippie-para-adulto.jpg.jpg")
+wagon = Costume.new(title:"wagon", user: julien, description: "déguisement pour les soirées du wagon", location: "Paris", price: 30, size: "M", gender: "M", theme: "other")
 wagon.photo.attach(io: file_wagon, filename: 'wagon.jpg', content_type: 'image/jpg')
+wagon.save!
+
 puts "Costumes created!"
 
 puts "Creating bookings..."
