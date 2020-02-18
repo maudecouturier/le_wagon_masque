ab6a84294fc6fad98a5b60d353745a0145105306
Alex Huyn <huyn.alex@gmail.com>
2020-02-18 15:56:51 +0100

    refonte booking card

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,27 +1,24 @@
 class ReviewsController < ApplicationController
-# a faire avec les views
-#   def new
-#     @booking = Booking.find(params[:id])
-#     @review = Review.new
-#   end
+  def new
+    @review = Review.new
+  end
 
-#   def create
-#     @review = Review.new(review_params)
-#     @review.user = current_user
-#     @review.booking = @booking
-#     if @review.save
-#       redirect_to booking_path(@booking)
-#     else
-#       render :new
-#     end
-#   end
+  def create
+    @review = Review.new(review_params)
+    @review.user = current_user
+    if @review.save
+      redirect_to my_bookings_path
+    else
+      render :new
+    end
+  end
 
-#   def destroy
-#   end
+  def destroy
+  end
 
-#   private
+  private
 
-#   def review_params
-#     params.require(:review).permit(:content)
-#   end
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   end
 
   resources :bookings, except: [:new, :create] do
-    resources :reviews, only: [:destroy]
+    resources :reviews, only: [:new, :create, :destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
