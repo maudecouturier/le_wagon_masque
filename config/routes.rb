d78a2c3020bf8871b3c4022a96ccec16a37bad02
Charlotte Margus <charlotte.boyce@hotmail.fr>
2020-02-21 15:33:54 +0100

    filter

--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,6 @@ Rails.application.routes.draw do
 
   resources :reviews, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
 end
