b91f10e2c73167c48e64bdb2c636597475a1690b
Alex Huyn <huyn.alex@gmail.com>
2020-02-21 15:57:21 +0100

    drag and drop

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,7 @@ import { initFlatpicker } from  "../components/init_flatpickr";
 import { initMapbox } from '../components/init_mapbox';
 import { initAutocomplete } from '../components/init_autocomplete';
 import { priceCalculation } from '../components/price_calculator';
+import { initDraggable } from '../components/draggable';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your JS functions here
@@ -22,5 +23,6 @@ document.addEventListener('turbolinks:load', () => {
   initAutocomplete();
   priceCalculation();
   $('.dropdown-toggle').dropdown();
+  initDraggable();
 });
 
